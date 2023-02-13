db29972abb41f1cacc447a961ad2f2ed3dfcee23
MrWiguna <anjayyotor@gmail.com>
2023-02-13 14:15:53 +0700

    push

---
+++ Test 1/index.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <title>A000124 of Sloane’s OEIS - MR Wiguna</title>
+    </head>
+    <body>
+        <?php
+        function caloeis($number){
+
+            return $numoeis;
+        }
+
+        if(isset($_GET['num_pengulangan'])){
+            $num_pengulangan = $_GET['num_pengulangan'];
+            if($num_pengulangan == ""){
+
+                echo "jangan kosong kaka";
+            }else{
+
+                for($i = 0; $i < $num_pengulangan; $i++){
+                    if($i != 0){
+                        echo "-";
+                        $number += $i;
+                    }else{
+                        $number = 1;
+                    }
+                    echo $number;
+                }
+            }
+        }?>
+
+        <form action="./" method="get">
+            <input type="number" name="num_pengulangan">
+            <button type="submit">Hitung</button>
+        </form>
+    </body>
+</html>
